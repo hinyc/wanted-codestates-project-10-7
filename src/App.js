5fcf174d83d5a510b64648b30d4aa5714fc30761
try <dlsduf@gmail.com>
2022-03-07 17:18:52 +0900

    [BoilerPlate] 공통 모듈 설치

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import Main from './pages/Main';
+import styled from 'styled-components';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <AllWrapper>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </AllWrapper>
     </Router>
   );
 }
 
 export default App;
+
+const AllWrapper = styled.div`
+  width: 1000px;
+`;
