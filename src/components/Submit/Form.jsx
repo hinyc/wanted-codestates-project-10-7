d448dd9dcfc091de414a745178e4c020e3fdb126
try <dlsduf@gmail.com>
2022-03-08 00:12:23 +0900

    [Feat] #21 #22 각각 데이터 관리가능하도록 리듀서 생성, 옵션아이디를 통해 선택적 이벤트 실행

--- src/components/Submit/Form.jsx
+++ src/components/Submit/Form.jsx
@@ -1,23 +1,48 @@
 import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { addSubmitData, initSubmitData } from '../../modules/submit';
 
 export default function Form({ data }) {
   const [showVerification, setShowVerification] = useState(false);
+  const [selectOption, setSelectOption] = useState(true);
+  const dispatch = useDispatch();
+
   const value = useRef('');
   const id = data.id;
   const type = data.type;
+  const submitData = useSelector((state) => state.submit);
 
-  const verificationHandler = (e) => {
+  const eventHandler = (e) => {
     const currentValue = value.current.value;
-    if (e.type === 'change') {
-      if (currentValue.length) setShowVerification(false);
-      else setShowVerification(true);
-    }
-    if (e.type === 'focus') {
-      if (!currentValue.length) setShowVerification(true);
+    switch (data.type) {
+      case 'text':
+        if (e.type === 'change') {
+          if (currentValue.length) setShowVerification(false);
+          else setShowVerification(true);
+        }
+        if (e.type === 'focus') {
+          if (!currentValue.length) setShowVerification(true);
+        }
+        break;
+      case 'select':
+        console.log(e.target.value);
+        dispatch(addSubmitData(data.id, e.target.value));
+        setSelectOption('');
+        console.log(submitData);
+
+        break;
+      default:
+        break;
     }
   };
 
+  const optionSelectHandler = (id) => {
+    console.log('click');
+    if (id !== selectOption) {
+      setSelectOption(id);
+    }
+  };
   //이벤트 블러 실행시 데이터 저장
 
   return (
@@ -27,8 +52,8 @@ export default function Form({ data }) {
           <label>{data.required ? data.label : `${data.label}(선택)`}</label>
           <input
             ref={value}
-            onChange={verificationHandler}
-            onFocus={verificationHandler}
+            onChange={eventHandler}
+            onFocus={eventHandler}
             placeholder={data.placeholder && data.placeholder}
           />
           {id === 'name' ? (
@@ -55,15 +80,26 @@ export default function Form({ data }) {
       {type === 'select' && (
         <form>
           <label>{data.required ? data.label : `${data.label}(선택)`}</label>
-          <div>
-            <select>
-              {data.options.map((el) => (
-                <option>{el}</option>
-              ))}
-            </select>
-          </div>
+          <Select>
+            <div
+              className="currentSelector"
+              onClick={() => optionSelectHandler(data.id)}
+            >
+              {submitData[data.id] || data.options[0]}
+            </div>
+            {selectOption === data.id && (
+              <OptionWrapper>
+                {data.options.map((el, idx) => (
+                  <option ref={value} key={idx} onClick={eventHandler}>
+                    {el}
+                  </option>
+                ))}
+              </OptionWrapper>
+            )}
+          </Select>
         </form>
       )}
+
       {type === 'file' && (
         <form>
           <label>{data.required ? data.label : `${data.label}(선택)`}</label>
@@ -141,7 +177,44 @@ const Container = styled.div`
   }
 `;
 
-const Select = styled.div``;
+const Select = styled.div`
+  margin-top: 10px;
+  width: 100%;
+  height: 48px;
+  outline: none;
+  background-color: #f8f9fb;
+  border-radius: 10px;
+  padding: 0 15px;
+  border: 1px solid #666;
+  border-radius: 10px 10px 0 0;
+  position: relative;
+  div.currentSelector {
+    width: 100%;
+    height: 100%;
+    line-height: 48px;
+    font-size: 18px;
+    font-weight: 700;
+  }
+`;
+const OptionWrapper = styled.div`
+  border: 1px solid #666;
+  border-radius: 0 0 10px 10px;
+  position: absolute;
+  top: 48px;
+  left: 0px;
+  background-color: #f7fafb;
+  option {
+    height: 48px;
+    line-height: 48px;
+    padding: 0 15px;
+    font-size: 18px;
+    font-weight: 700;
+    :hover {
+      background-color: #e0e5e8;
+    }
+  }
+`;
+
 const Agreement = styled.div`
   font-size: 16px;
   font-weight: 700;
