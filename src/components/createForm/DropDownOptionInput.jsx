2eb9f50407d61ac93e8c6e044b613af02c74b796
juo <hwgiwh271@gmail.com>
2022-03-08 18:41:45 +0900

    [Feat] #41 자동 토큰 분리하기
    -

--- src/components/createForm/DropDownOptionInput.jsx
+++ src/components/createForm/DropDownOptionInput.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
 export default function DropDownOptionInput() {
-  const [options, setOptions] = useState({ text: null, id: uuidv4() });
-  const [value, setValue] = useState(null);
+  const [options, setOptions] = useState([{ text: null, id: uuidv4() }]);
+  const [value, setValue] = useState('');
 
   const Parser = class {
     test() {
@@ -14,7 +14,7 @@ export default function DropDownOptionInput() {
     }
   };
   const CommaParser = class extends Parser {
-    #reg = '/([^,]*),/';
+    #reg = /([^,]*),/;
     test(value) {
       return this.#reg.test(value);
     }
@@ -39,21 +39,84 @@ export default function DropDownOptionInput() {
         return result;
     },
   };
-
   const setOnValue = (e) => {
     const value = e.target.value;
     const res = reg.convert(value);
     if (!res) setValue(e.target.value);
     else {
-      console.log(res);
+      const mathcedStr = res[1];
+      setOptions((prevOption) => [
+        ...prevOption,
+        { text: mathcedStr, id: uuidv4() },
+      ]);
+      setValue('');
     }
   };
+  const deleteBlock = (id) => {
+    setOptions(options.filter((opt) => opt.id !== id));
+  };
   return (
-    <input
-      type="text"
-      placeholder="옵션 (각 아이템은 콤마(,)로 구분합니다.)"
-      value={value}
-      onChange={setOnValue}
-    />
+    <Container>
+      {options.map(({ text, id }) => {
+        return (
+          <>
+            {text && (
+              <Block key={id}>
+                <Text>{text}</Text>
+                <CloseBtn onClick={() => deleteBlock(id)}>&times;</CloseBtn>
+              </Block>
+            )}
+          </>
+        );
+      })}
+      <Input
+        type="text"
+        placeholder=", 로 구분"
+        value={value}
+        onChange={setOnValue}
+      />
+    </Container>
   );
 }
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 500px;
+  height: 36px;
+  padding: 0 10px;
+  border-top: 1px solid #f2f2f2;
+  overflow-x: auto;
+  margin: 0 -10px;
+`;
+const Block = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  height: 30px;
+  border: 3px solid #f8be6c;
+  margin: 3px;
+  border-radius: 5px;
+`;
+const Text = styled.span`
+  color: #f8be6c;
+  font-weight: bold;
+  padding: 15px;
+`;
+const CloseBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #8e590e;
+  position: absolute;
+  right: 1px;
+  top: 1px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+const Input = styled.input`
+  height: 100%;
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 15px;
+  border-radius: 5px;
+`;
