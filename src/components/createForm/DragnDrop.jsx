104a7144ddd5efbfad1c9e74fa1689b9f5ca49fe
Space-Belt <dngurwn@gmail.com>
2022-03-08 20:53:24 +0900

    [Fix] 필요없는 코드 삭제

--- src/components/createForm/DragnDrop.jsx
+++ src/components/createForm/DragnDrop.jsx
@@ -8,17 +8,13 @@ function DragnDrop() {
     { title: 'Line' },
     { title: 'Instagram' },
     { title: 'Telegram' },
-    // { title: 'KaKao' },
-    // { title: 'LinkedIn' },
+    { title: 'KaKao' },
+    { title: 'LinkedIn' },
   ];
   const [snsList, setSnsList] = useState(socialNetworks);
   const [grab, setGrab] = useState();
   const [pickedIndex, setPickedIndex] = useState();
-  // const [firstClicked, setFirstClicked] = useState();
-  // const handleOnclick = (e) => {
-  //   setFirstClicked(e.pageY);
-  //   console.log(firstClicked);
-  // };
+
   const onDragStart = (e) => {
     setGrab(e.target.innerText);
     setPickedIndex(e.target);
@@ -26,19 +22,14 @@ function DragnDrop() {
   const onDragOver = (e, title) => {
     e.preventDefault();
     if (title === grab) return;
-    // console.log(e.target);
   };
-  const onDragEnd = (e) => {
-    // e.preventDefault();
-    // console.log('pageY', e.pageY);
-  };
+  const onDragEnd = (e) => {};
   const onDrop = (e) => {
     e.preventDefault();
     let pickedPosition = Number(pickedIndex.dataset.position);
     let dropedPosition = Number(e.target.dataset.position);
     const target = e.target.innerText;
     const endPoint = e.target.offsetTop + e.target.offsetHeight / 2;
-    console.log('엔드포인트입니다 ' + endPoint);
     let targetIndex;
     const filteredSns = snsList.filter((sns) => sns.title !== grab);
     snsList.forEach((sns, i) => {
@@ -46,10 +37,7 @@ function DragnDrop() {
         targetIndex = i;
       }
     });
-    console.log('타겟인덱스 ' + targetIndex);
-    console.log('마지막마우스위치 ' + e.pageY);
 
-    // 엔드포인트 보다 작을때 endPoint - e.target.offsetHeight / 2 < e.pageY &&
     if (pickedPosition < dropedPosition) {
       if (e.pageY < endPoint) {
         // 자기 영역에서 실행되면 실행 안되게
@@ -60,16 +48,6 @@ function DragnDrop() {
         setSnsList(filteredSns);
         return;
       }
-      // 엔드포인트 보다 클때 e.pageY < endPoint + e.target.offsetHeight / 2 &&
-      // if (e.pageY < endPoint) {
-      //   // 자기 영역에서 실행되면 실행 안되게
-      //   if (pickedPosition === dropedPosition) {
-      //     return;
-      //   }
-      // 자기 인덱스보다 하나 작은애 한테 하면 안먹도록
-      // if (pickedPosition - 1 === dropedPosition) {
-      //   return;
-      // }
 
       filteredSns.splice(Number(targetIndex), 0, { title: grab });
       setSnsList(filteredSns);
