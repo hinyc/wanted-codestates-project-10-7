df392a700d8adb655003aed5d1efdce0b5de4ade
Space-Belt <dngurwn@gmail.com>
2022-03-08 20:49:51 +0900

    [Feat] #2 Drag and Drop 기능구현

--- src/components/createForm/DragnDrop.jsx
+++ src/components/createForm/DragnDrop.jsx
@@ -8,13 +8,20 @@ function DragnDrop() {
     { title: 'Line' },
     { title: 'Instagram' },
     { title: 'Telegram' },
-    { title: 'KaKao' },
-    { title: 'LinkedIn' },
+    // { title: 'KaKao' },
+    // { title: 'LinkedIn' },
   ];
   const [snsList, setSnsList] = useState(socialNetworks);
   const [grab, setGrab] = useState();
+  const [pickedIndex, setPickedIndex] = useState();
+  // const [firstClicked, setFirstClicked] = useState();
+  // const handleOnclick = (e) => {
+  //   setFirstClicked(e.pageY);
+  //   console.log(firstClicked);
+  // };
   const onDragStart = (e) => {
     setGrab(e.target.innerText);
+    setPickedIndex(e.target);
   };
   const onDragOver = (e, title) => {
     e.preventDefault();
@@ -27,8 +34,11 @@ function DragnDrop() {
   };
   const onDrop = (e) => {
     e.preventDefault();
+    let pickedPosition = Number(pickedIndex.dataset.position);
+    let dropedPosition = Number(e.target.dataset.position);
     const target = e.target.innerText;
     const endPoint = e.target.offsetTop + e.target.offsetHeight / 2;
+    console.log('엔드포인트입니다 ' + endPoint);
     let targetIndex;
     const filteredSns = snsList.filter((sns) => sns.title !== grab);
     snsList.forEach((sns, i) => {
@@ -36,13 +46,46 @@ function DragnDrop() {
         targetIndex = i;
       }
     });
-    if (e.pageY < endPoint) {
-      filteredSns.splice(targetIndex, 0, { title: grab });
+    console.log('타겟인덱스 ' + targetIndex);
+    console.log('마지막마우스위치 ' + e.pageY);
+
+    // 엔드포인트 보다 작을때 endPoint - e.target.offsetHeight / 2 < e.pageY &&
+    if (pickedPosition < dropedPosition) {
+      if (e.pageY < endPoint) {
+        // 자기 영역에서 실행되면 실행 안되게
+        if (pickedPosition === dropedPosition) {
+          return;
+        }
+        filteredSns.splice(Number(targetIndex) - 1, 0, { title: grab });
+        setSnsList(filteredSns);
+        return;
+      }
+      // 엔드포인트 보다 클때 e.pageY < endPoint + e.target.offsetHeight / 2 &&
+      // if (e.pageY < endPoint) {
+      //   // 자기 영역에서 실행되면 실행 안되게
+      //   if (pickedPosition === dropedPosition) {
+      //     return;
+      //   }
+      // 자기 인덱스보다 하나 작은애 한테 하면 안먹도록
+      // if (pickedPosition - 1 === dropedPosition) {
+      //   return;
+      // }
+
+      filteredSns.splice(Number(targetIndex), 0, { title: grab });
       setSnsList(filteredSns);
       return;
     }
-    if (targetIndex !== snsList.length - 2) {
-      filteredSns.splice(targetIndex + 1, 0, { title: grab });
+
+    if (pickedPosition > dropedPosition) {
+      if (e.pageY < endPoint) {
+        if (pickedPosition === dropedPosition) {
+          return;
+        }
+        filteredSns.splice(Number(targetIndex), 0, { title: grab });
+        setSnsList(filteredSns);
+        return;
+      }
+      filteredSns.splice(Number(targetIndex) + 1, 0, { title: grab });
       setSnsList(filteredSns);
       return;
     }
@@ -54,6 +97,7 @@ function DragnDrop() {
         {snsList.map((sns, index) => (
           <List
             draggable
+            data-position={index}
             key={index}
             onDragOver={(e) => onDragOver(e, sns.title)}
             onDragStart={onDragStart}
