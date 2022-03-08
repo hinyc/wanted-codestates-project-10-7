a550ed9de0d6fd8aa32ef0216fb08c069c1911ed
try <dlsduf@gmail.com>
2022-03-08 11:02:54 +0900

    [Feat]#3 이름, 번호 상태저장 추가

--- src/components/Submit/Form.jsx
+++ src/components/Submit/Form.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { addSubmitData, initSubmitData } from '../../modules/submit';
 
 export default function Form({ data }) {
   const [showVerification, setShowVerification] = useState(false);
-  const [selectOption, setSelectOption] = useState(true);
+  const [showOption, setShowOption] = useState('');
+  const [agreementState, setAgreementState] = useState(false);
   const dispatch = useDispatch();
 
   const value = useRef('');
@@ -24,14 +25,21 @@ export default function Form({ data }) {
         if (e.type === 'focus') {
           if (!currentValue.length) setShowVerification(true);
         }
+        if (e.type === 'blur' && !showVerification) {
+          dispatch(addSubmitData(data.id, e.target.value));
+        }
+        break;
+      case 'phone':
+        const num = e.target.value;
+        console.log(num);
+        dispatch(addSubmitData(data.id, e.target.value));
         break;
       case 'select':
-        console.log(e.target.value);
         dispatch(addSubmitData(data.id, e.target.value));
-        setSelectOption('');
-        console.log(submitData);
+        setShowOption('');
 
         break;
+
       default:
         break;
     }
@@ -39,11 +47,16 @@ export default function Form({ data }) {
 
   const optionSelectHandler = (id) => {
     console.log('click');
-    if (id !== selectOption) {
-      setSelectOption(id);
+    console.log(id, showOption);
+    if (id !== showOption) {
+      setShowOption(id);
     }
   };
   //이벤트 블러 실행시 데이터 저장
+  const backgroundHandler = () => {
+    // 정상 기능동작 않함, 수정필요
+    setShowOption('');
+  };
 
   return (
     <Container>
@@ -54,6 +67,7 @@ export default function Form({ data }) {
             ref={value}
             onChange={eventHandler}
             onFocus={eventHandler}
+            onBlur={eventHandler}
             placeholder={data.placeholder && data.placeholder}
           />
           {id === 'name' ? (
@@ -67,7 +81,7 @@ export default function Form({ data }) {
       {type === 'phone' && (
         <form>
           <label>{data.required ? data.label : `${data.label}(선택)`}</label>
-          <input />
+          <input onBlur={eventHandler} />
         </form>
       )}
 
@@ -80,14 +94,15 @@ export default function Form({ data }) {
       {type === 'select' && (
         <form>
           <label>{data.required ? data.label : `${data.label}(선택)`}</label>
-          <Select>
+          <Select id={data.id} showOption={showOption}>
             <div
               className="currentSelector"
               onClick={() => optionSelectHandler(data.id)}
             >
               {submitData[data.id] || data.options[0]}
             </div>
-            {selectOption === data.id && (
+            <BackGround onClick={backgroundHandler} />
+            {showOption === data.id && (
               <OptionWrapper>
                 {data.options.map((el, idx) => (
                   <option ref={value} key={idx} onClick={eventHandler}>
@@ -109,9 +124,11 @@ export default function Form({ data }) {
 
       {type === 'agreement' && (
         <Agreement>
-          <div className="check"></div>
-          <div>{`${data.label} (필수)`}</div>
-          <div className="arrow"></div>
+          <div className="miniWrap">
+            <div className="check"></div>
+            <div className="text">{`${data.label} (필수)`}</div>
+          </div>
+          <div className="arrow">&gt;</div>
         </Agreement>
       )}
     </Container>
@@ -130,11 +147,9 @@ const Container = styled.div`
   label {
     font-weight: 700;
     font-size: 12px;
-    width: 100%;
+    /* width: 100%; */
   }
-  div {
-    width: 100%;
-  }
+
   div.verification {
     height: 12px;
     font-size: 12px;
@@ -168,9 +183,9 @@ const Container = styled.div`
     }
   }
 
-  input.file {
+  .file {
     width: 90%;
-    height: 200px;
+    height: 100px;
     :hover {
       cursor: pointer;
     }
@@ -179,29 +194,35 @@ const Container = styled.div`
 
 const Select = styled.div`
   margin-top: 10px;
-  width: 100%;
   height: 48px;
   outline: none;
   background-color: #f8f9fb;
   border-radius: 10px;
   padding: 0 15px;
-  border: 1px solid #666;
-  border-radius: 10px 10px 0 0;
   position: relative;
   div.currentSelector {
-    width: 100%;
     height: 100%;
     line-height: 48px;
     font-size: 18px;
     font-weight: 700;
   }
+  ${({ id, showOption }) => {
+    if (id === showOption) {
+      return css`
+        border-radius: 10px 10px 0 0;
+        border: 1px solid #666;
+      `;
+    }
+  }}
 `;
+
 const OptionWrapper = styled.div`
+  width: 500px;
   border: 1px solid #666;
   border-radius: 0 0 10px 10px;
   position: absolute;
-  top: 48px;
-  left: 0px;
+  top: 46px;
+  left: -1px;
   background-color: #f7fafb;
   option {
     height: 48px;
@@ -215,7 +236,37 @@ const OptionWrapper = styled.div`
   }
 `;
 
+const BackGround = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0, 0, 0, 0);
+  width: 100vw;
+  height: 100vh;
+  z-index: -2;
+`;
+
 const Agreement = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
   font-size: 16px;
   font-weight: 700;
+  .miniWrap {
+    display: flex;
+    align-items: center;
+  }
+  div.check {
+    width: 20px;
+    height: 20px;
+    border: 1px solid #d6d9dc;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+  div.arrow {
+    color: #8d959d;
+    font-size: 20px;
+  }
 `;
